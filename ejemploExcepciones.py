8602a8d6df83fb734f436a1bfc876ef020a81eed
SamuelRiveira <samuelriveiraescudero@gmail.com>
2024-04-17 09:57:20 +0100

    Creado ejemploExcepciones y mi_archivo.txt (ejemplo de error)

--- ejemploExcepciones.py
+++ ejemploExcepciones.py
@@ -1,18 +1,82 @@
 class EjemploExcepciones:
-    #ZeroDivisionError
+    def ZeroDivisionError():
+        a = 5
+        b = 0
+        if (a == 0) or (b == 0):
+            raise ZeroDivisionError
+        return a / b
 
-    #ValueError
+    def ValueError():    
+        numEntero = "Hola"
+        if type(numEntero) != int:
+            raise ValueError
+        return numEntero
 
-    #FileNotFoundError
+    def FileNotFoundError():
+        with open('archivo_que_no_existe.txt', 'r') as f:
+            contenido = f.read()
+            if f.read():
+                raise FileNotFoundError
+            return contenido
 
-    #TypeError
+    def TypeError():
+        a = "7"
+        b = 4
+        if type(a) != int or type(b) != int:
+            raise TypeError
+        return a + b
 
-    #PermissionError
+    def PermissionError():
+        with open('mi_archivo.txt', 'r') as archivo:
+            contenido = archivo.read()
+            if archivo.read():
+                raise PermissionError
+            return contenido
 
-    #IndexError
+    def IndexError():
+        pass
 
-    #KeyboardInterrupt
+    def KeyboardInterrupt():
+        pass
 
-    #UnicodeDecodeError
+    def UnicodeDecodeError():
+        pass
 
-    #AttributeError
+    def AttributeError():
+        pass
+
+"""#ZeroDivisionError
+ejemplo = EjemploExcepciones.ZeroDivisionError()
+print(ejemplo)"""
+
+"""#ValueError
+ejemplo = EjemploExcepciones.ValueError()
+print(ejemplo)"""
+
+"""#FileNotFoundError
+ejemplo = EjemploExcepciones.FileNotFoundError()
+print(ejemplo)"""
+
+"""#TypeError
+ejemplo = EjemploExcepciones.TypeError()
+print(ejemplo)"""
+
+#PermissionError
+ejemplo = EjemploExcepciones.PermissionError()
+print(ejemplo)
+
+#IndexError
+ejemplo = EjemploExcepciones.IndexError()
+print(ejemplo)
+
+#KeyboardInterrupt
+ejemplo = EjemploExcepciones.KeyboardInterrupt()
+print(ejemplo)
+
+#UnicodeDecodeError
+ejemplo = EjemploExcepciones.UnicodeDecodeError()
+print(ejemplo)
+
+#AttributeError
+ejemplo = EjemploExcepciones.AttributeError()
+print(ejemplo)
